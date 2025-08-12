8a7e5d83acb1bfdbee69ea9db461b39f81100e03
akansha0009 <chouhanakansha09@gamil.com>
2025-08-12 11:08:35 +0530

    updated cards and deposit modal

--- src/components/ColorGame.tsx
+++ src/components/ColorGame.tsx
@@ -5,7 +5,7 @@ import AuthModal from "./AuthModal";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
 import Wingo from "../assets/Wingo.jpg";
-import WingoTRX from "../assets/Wingo.jpg"
+import WingoTRX from "../assets/Wingo.jpg";
 
 const ColorGame = () => {
   const auth = useSelector((state: RootState) => state.auth.isAuthenticated);
@@ -28,13 +28,16 @@ const ColorGame = () => {
     <>
       {/* Mobile View */}
       <section className="md:hidden h-full w-full ">
-        <div className="w-full max-w-[200px] mx-auto rounded-xl overflow-hidden relative group" style={{ height: "180px" }}>
+        <div
+          className="w-full max-w-[200px] mx-auto rounded-xl overflow-hidden relative group"
+          style={{ height: "200px" }}
+        >
           <img
             src={WingoTRX}
             alt="WinGo TRX"
             className="w-full h-full object-cover rounded-xl"
           />
-          
+
           {/* Overlay with Play button - shown on hover */}
           <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl flex items-center justify-center">
             <button
@@ -55,7 +58,7 @@ const ColorGame = () => {
             alt="WinGo TRX"
             className="w-full h-full object-cover rounded-2xl"
           />
-          
+
           {/* Overlay with Play Now button - shown on hover */}
           <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-2xl flex items-center justify-center">
             <button
