a67332af7141e13889694c878eaaa359dcb8d1a5
akansha0009 <chouhanakansha09@gamil.com>
2025-08-20 14:57:58 +0530

    wingo game cards and win/loss pop up issue resolved

--- src/components/ChickenRoad2.tsx
+++ src/components/ChickenRoad2.tsx
@@ -79,7 +79,7 @@ const ChickenRoad2 = () => {
       {/* Mobile View */}
       <section className="md:hidden h-full w-full">
         <div
-          className="w-full max-w-[200px] mx-auto rounded-xl overflow-hidden relative group"
+          className="w-full max-w-[180px] mx-auto rounded-xl overflow-hidden relative group"
           style={{ height: "200px" }}
         >
           <img
